5538aead98138fcb7dc52a09e571621dc1a5299c
ShailMurtaza <shailmurtaza@gmail.com>
2023-11-11 18:43:12 +0500

    Bal_animation with_sound: Audio balance done

--- BallAnimation/with_sounds/animation.js
+++ BallAnimation/with_sounds/animation.js
@@ -4,11 +4,13 @@ canvas.height = window.innerHeight - 35;
 
 var c = canvas.getContext("2d");
 
-var radius = 23
+var radius = 10
 var x = radius
 var y = radius
 var dx = 6.42
-var dy = 6
+var dy = 15
+//dx = 5
+//dy = 5
 //var dx = 7
 //var dy = 8
 //var dx = 10
@@ -49,35 +51,50 @@ class Circle {
     }
 }
 
-function ball_sound() {
-    var audio = new Audio('ball.mp3');
+function ball_sound(balance) {
+    const audio = new Audio('ball.mp3')
+    const audioCtx = new AudioContext()
+    const track = audioCtx.createMediaElementSource(audio)
+    const stereoNode = new StereoPannerNode(audioCtx, { pan: balance })
+    track.connect(stereoNode).connect(audioCtx.destination)
     audio.play();
 }
 
 
-const ball = new Circle(0, 0, radius, "rgba(0, 0, 0, 0)", "black")
+const global_ball = new Circle(0, 0, radius, "rgba(0, 0, 0, 0)", "black")
 
 function Animate() {
     requestAnimationFrame(Animate)
-    ball.fillColor = `rgba(${global_color[0].c}, ${global_color[1].c}, ${global_color[2].c}, 1)`
-    ball.move(x, y)
-    ball.draw()
+    global_ball.fillColor = `rgba(${global_color[0].c}, ${global_color[1].c}, ${global_color[2].c}, 1)`
+    global_ball.move(x, y)
+    global_ball.draw()
 
     x += dx
     y += dy
 
     // Bounce
+    let collision = false
     if( x + radius > canvas.width || x < radius) {
         dx = -dx
-        ball_sound()
+        collision = true
     }
     if( y + radius > canvas.height || y < radius) {
         dy = -dy
-        ball_sound()
+        collision = true
+    }
+    if (collision) {
+        let balance = calc_balance()
+        ball_sound(balance)
     }
 
 
     // Change colors
+    ch_color()
+    // console.log(global_color[0].c, global_color[1].c, global_color[2].c, global_color_ic)
+}
+
+
+function ch_color() {
     let my_color = global_color[global_color_ic]
     let new_color = my_color.c + my_color.i
     if (new_color > 255 || new_color < 0) {
@@ -86,9 +103,17 @@ function Animate() {
         global_color_ic = (global_color_ic+1) % 3
     }
     my_color.c = new_color
-    // console.log(global_color[0].c, global_color[1].c, global_color[2].c, global_color_ic)
 }
 
- Animate()
 
+function calc_balance() {
+    let full_width = canvas.width
+    // Left side of balance is not properly measured because radius is being added in x position.
+    // But is is good enough for now.
+    let balance = ((global_ball.x+global_ball.radius) * 2/full_width) - 1
+    return balance
+}
+
+
+document.body.addEventListener("click", Animate,{once:true})
 
